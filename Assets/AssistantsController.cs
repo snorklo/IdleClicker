063f65e3a64cc11ff7bbedc2b25b3ef62754187e
Sandra Jasionowska <sandra.jasionowska@boombit.com>
2019-11-16 19:47:52 +0100

    AssistantsController added, not working

---
+++ Assets/AssistantsController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class Assistant
+{
+    private string id;
+    private double initialPrice;
+
+
+    public string Id
+    {
+        get => id;
+        internal set => id = value;
+    }
+   
+
+    private double initialMoneyPerSecond;
+
+    private double moneyPerSecondFromOne;
+
+    private double price;
+
+    private double moneyPerSecond;
+
+    private double amountToBuy;
+
+    private double assistantsOwned;
+    
+    
+
+ 
+}
+
+/// <summary>
+/// Manages data about all assistans.
+/// </summary>
+public class AssistantsController
+{
+    private List<Assistant> assistants = new List<Assistant>();
+    Assistant a1 = new Assistant();
+
+    private void Initialize()
+    {
+        a1.Id
+    }
+    
+}
